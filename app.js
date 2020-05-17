c55bcfdfabe92f5d7f51d41db7a05741b1f57c1b
khushboo <khushboovjvrg018@gmail.com>
2020-05-17 22:22:52 +0530

    two new file

---
+++ app.js
@@ -0,0 +1,9 @@
+const math =require('./math');
+
+/*function displayName(name){
+	console.log("my name is "+name)
+}
+displayName('khushboo')
+console.log(module)*/
+console.log("my addition: "+math.sum(5,5))
+console.log("my multiplication: "+math.multiply(5,3))
